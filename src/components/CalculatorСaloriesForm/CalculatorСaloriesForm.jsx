9d9ed064029f4d7c32a02824e72ebb7ccedd2e56
Serhii Manko <msa01051984@gmail.com>
2023-02-02 09:02:57 +0200

    inpit-radio

--- src/components/CalculatorСaloriesForm/CalculatorСaloriesForm.jsx
+++ src/components/CalculatorСaloriesForm/CalculatorСaloriesForm.jsx
@@ -3,8 +3,8 @@ import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchCalorie } from 'redux/authModal/modalThunk';
 import css from './CalculatorСaloriesForm.module.css';
-
 function CalculatorСaloriesForm() {
+  const [selectedOption, setSelectedOption] = useState('1');
   const [formData, setFormData] = useState({
     height: '',
     age: '',
@@ -12,19 +12,10 @@ function CalculatorСaloriesForm() {
     desiredWeight: '',
     bloodType: '',
   });
-
   const dispatch = useDispatch();
   const [isModalOpen, setIsOpen] = useState(false);
   const openModal = () => setIsOpen(true);
   const toggle = () => setIsOpen(isOpen => !isOpen);
-
-  const handleChange = event => {
-    setFormData(prevState => ({
-      ...prevState,
-      [event.target.name]: event.target.value,
-    }));
-  };
-
   const handleSubmit = event => {
     event.preventDefault();
     const formData = { weight, height, age, desiredWeight, bloodType };
@@ -37,85 +28,125 @@ function CalculatorСaloriesForm() {
       bloodType: '',
     });
   };
-
   const { weight, height, age, desiredWeight, bloodType } = formData;
-
   return (
     <>
-    <form className={css.formCalculate} onSubmit={handleSubmit}>
-      <div className={css.wrapper}>
-        <div className={css.wrapperLeft}>
-          <label className={css.labelCalculate}>
-            Height *
-            <input className={css.inputCalculate}  required type="text" name="height" value={height} onChange={handleChange}/>
-          </label>
-          <label className={css.labelCalculate}>
-            Age *
-            <input className={css.inputCalculate} required type="text" name="age" value={age} onChange={handleChange}/>
-          </label>
-          <label className={css.labelCalculate}>
-            Current weight *
-            <input className={css.inputCalculate} required type="text" name="weight" value={weight} onChange={handleChange}/>
-          </label>
-        </div>
-        <div className={css.wrapperRight}>
-          <label className={css.labelCalculate}>
-            Desired weight *
-            <input className={css.inputCalculate} required type="text" name="desiredWeight" value={desiredWeight} onChange={handleChange}/>
-          </label>
-          <div className={css.radio}>
-            <p className={css.radioTitle}>Blood type *</p>
-            <label className={css.labelRadio}>
+      <form className={css.formCalculate} onSubmit={handleSubmit}>
+        <div className={css.wrapper}>
+          <div className={css.wrapperLeft}>
+            <label className={css.labelCalculate}>
+              Height *
               <input
-                className={css.inputRadio}
-                type="radio"
-                name="bloodType"
-                value="1"
-                checked
-                onChange={handleChange}
+                className={css.inputCalculate}
+                required
+                type="text"
+                name="height"
+                value={height}
+                onChange={e =>
+                  setFormData({ ...formData, height: e.target.value })
+                }
               />
-              <span className={css.inputRadioStyled}></span>1
             </label>
-            <label className={css.labelRadio}>
+            <label className={css.labelCalculate}>
+              Age *
               <input
-                className={css.inputRadio}
-                type="radio"
-                name="bloodType"
-                value="2"
-                onChange={handleChange}
+                className={css.inputCalculate}
+                required
+                type="text"
+                name="age"
+                value={age}
+                onChange={e =>
+                  setFormData({ ...formData, age: e.target.value })
+                }
               />
-              <span className={css.inputRadioStyled}></span>2
             </label>
-            <label className={css.labelRadio}>
+            <label className={css.labelCalculate}>
+              Current weight *
               <input
-                className={css.inputRadio}
-                type="radio"
-                name="bloodType"
-                value="3"
-                onChange={handleChange}
+                className={css.inputCalculate}
+                required
+                type="text"
+                name="weight"
+                value={weight}
+                onChange={e =>
+                  setFormData({ ...formData, weight: e.target.value })
+                }
               />
-              <span className={css.inputRadioStyled}></span>3
-            </label>
-            <label className={css.labelRadio}>
-              <input
-                className={css.inputRadio}
-                type="radio"
-                name="bloodType"
-                value="4"
-                onChange={handleChange}
-              />
-              <span className={css.inputRadioStyled}></span>4
             </label>
           </div>
+          <div className={css.wrapperRight}>
+            <label className={css.labelCalculate}>
+              Desired weight *
+              <input
+                className={css.inputCalculate}
+                required
+                type="text"
+                name="desiredWeight"
+                value={desiredWeight}
+                onChange={e =>
+                  setFormData({ ...formData, desiredWeight: e.target.value })
+                }
+              />
+            </label>
+            <div className={css.radio}>
+              <p className={css.radioTitle}>Blood type *</p>
+              <label className={css.labelRadio}>
+                1
+                <input
+                  className={css.inputRadio}
+                  type="radio"
+                  name="bloodType"
+                  value="1"
+                  checked={selectedOption === '1'}
+                  onChange={e => setSelectedOption(e.target.value)}
+                />
+                <span className={css.inputRadioStyled}></span>
+              </label>
+              <label className={css.labelRadio}>
+                2
+                <input
+                  className={css.inputRadio}
+                  type="radio"
+                  name="bloodType"
+                  value="2"
+                  checked={selectedOption === '2'}
+                  onChange={e => setSelectedOption(e.target.value)}
+                />
+                <span className={css.inputRadioStyled}></span>
+              </label>
+              <label className={css.labelRadio}>
+                3
+                <input
+                  className={css.inputRadio}
+                  type="radio"
+                  name="bloodType"
+                  value="3"
+                  checked={selectedOption === '3'}
+                  onChange={e => setSelectedOption(e.target.value)}
+                />
+                <span className={css.inputRadioStyled}></span>
+              </label>
+              <label className={css.labelRadio}>
+                4
+                <input
+                  className={css.inputRadio}
+                  type="radio"
+                  name="bloodType"
+                  value="4"
+                  checked={selectedOption === '4'}
+                  onChange={e => setSelectedOption(e.target.value)}
+                />
+                <span className={css.inputRadioStyled}></span>
+              </label>
+            </div>
+          </div>
         </div>
-      </div>
-      <button className={css.btnCalculate} onClick={openModal} type="submit">
-        Start losing weight
-      </button>
-    </form>
-    {isModalOpen && <Modal closeModal={toggle} />}
+        <button className={css.btnCalculate} onClick={openModal} type="submit">
+          Start losing weight
+        </button>
+      </form>
+      {isModalOpen && <Modal closeModal={toggle} />}
     </>
   );
 }
-
 export default CalculatorСaloriesForm;
