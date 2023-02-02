29b96b6601b5fbb6022448c94ee9fa08ef256511
Aleksey <avralgroup@gmail.com>
2023-02-02 10:08:17 +0200

    add to rightSideBar

--- src/components/RightSideBar/RightSideBar.jsx
+++ src/components/RightSideBar/RightSideBar.jsx
@@ -3,24 +3,38 @@ import { useSelector } from 'react-redux';
 import css from './RightSideBar.module.css';
 
 export const RightSideBar = () => {
-  const kcal = useSelector(state => state.infoUser?.daySummary?.kcalConsumed)
-  return (
+  const kcalLeft = useSelector(state => state?.infoUser?.daySummary?.kcalLeft);
 
+  const kcalConsumed = useSelector(
+    state => state?.infoUser?.daySummary?.kcalConsumed
+  );
+
+  const dailyRate = useSelector(
+    state => state?.infoUser?.daySummary?.dailyRate
+  );
+
+  const percentsOfDailyRate = useSelector(
+    state =>
+      state?.infoUser?.daySummary?.percentsOfDailyRate);
+
+  return (
     <div className={css.containerTitle}>
       <div className={css.summaryRightSideBar}>
         <h3 className={css.headerRightSideBar}>Summary for 20/06/2020</h3>
         <ul className={css.listRightSideBar}>
           <li className={css.itemRihgtSideBar}>
-            Left <span className={css.spanRightSideBar}>000 kcal</span>
+            Left <span className={css.spanRightSideBar}>{kcalLeft} kcal</span>
           </li>
           <li className={css.itemRihgtSideBar}>
-            Consumed <span className={css.spanRightSideBar}>{kcal} kcal</span>
+            Consumed{' '}
+            <span className={css.spanRightSideBar}>{kcalConsumed} kcal</span>
           </li>
           <li className={css.itemRihgtSideBar}>
-            Daily rate <span className={css.spanRightSideBar}>000 kcal</span>
+            Daily rate{' '}
+            <span className={css.spanRightSideBar}>{dailyRate} kcal</span>
           </li>
           <li className={css.itemRihgtSideBar}>
-            n% of normal <span className={css.spanRightSideBar}>000 kcal</span>
+            n% of normal <span className={css.spanRightSideBar}>{percentsOfDailyRate} kcal</span>
           </li>
         </ul>
       </div>
@@ -37,6 +51,5 @@ export const RightSideBar = () => {
           </ul> */}
       </div>
     </div>
-
   );
 };
